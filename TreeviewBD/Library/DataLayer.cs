0c168ce96396a53b02eea25929b6ba9b69062445
Rafael Sanzio <rafaelsanzio16@gmail.com>
2019-01-29 11:21:36 -0200

    "commit"

---
+++ TreeviewBD/Library/DataLayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Library
+{
+    public static class DataLayer
+    {
+        public static DataTable GetUserDepartment(string userLogin)
+        {
+            DataTable datatable = new DataTable();
+            string command = string.Format("select * from SIMPLAD_DEPARTMENT t1 inner join SIMPLAD_DEPARTMENT_USER t2 on t1.DepartmentID = t2.DepartmentID where t2.UserID = '{0}' ", userLogin);
+
+            using (SqlConnection sqlConnection = CreateConnection("ITAU_SSMP"))
+            {
+                using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command, sqlConnection))
+                {
+                    sqlDataAdapter.Fill(datatable);
+                }
+            }
+
+            return datatable;
+        }
+
+        public static DataTable GetComputerUser(string userLogin)
+        {
+            DataTable datatable = new DataTable();
+            string command = string.Format("select * from RUM_COMPUTER where UserLogon = '{0}'", userLogin);
+
+            using (SqlConnection sqlConnection = CreateConnection("SELF_MIGRATE2"))
+            {
+                using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command, sqlConnection))
+                {
+                    sqlDataAdapter.Fill(datatable);
+                }
+            }
+
+            return datatable;
+        }
+
+        private static SqlConnection CreateConnection(string connKey)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[connKey].ConnectionString;
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            return sqlConnection;
+        }
+
+
+    }
+}
---
+++ TreeviewBD/TreeviewBD/home.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TreeviewBD
+{
+	public partial class home : System.Web.UI.Page
+	{
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            if (!IsPostBack)
+            {
+                TreeNode node = new TreeNode();
+                node.ImageUrl = "~/imagens/GRUPO.png";
+                node.Text = "CONTOSO";
+                node.Value = "DC=CONTOSO,DC=LOCAL";
+                arvore.Nodes.Add(node);
+            }
+        }
+
+        public string[] GetOUs(string path)
+
+        {
+            List<string> obj = new List<string>(); //nao precisa de tamanho fixo
+                                                   // List<string> users = new List<string>();
+
+
+            DirectoryEntry entry = new DirectoryEntry("LDAP://192.168.92.200/" + path, "contoso\\administrator", "Br@sil01");
+
+            foreach (DirectoryEntry child in entry.Children)//cria uma var que vai navegar pelos filhos do AD
+            {
+                if (child.SchemaClassName == "organizationalUnit") //seleciona apenas o tipo OU
+                {
+                    obj.Add(child.Path.Split('/')[3]); //seleciona um separador, passando o index do que vc quer retornar
+
+
+                }
+
+                else if (child.SchemaClassName == "user")
+                {
+
+                    obj.Add(child.Path.Split('/')[3]);
+                }
+            }
+
+            entry.Dispose();
+            return obj.ToArray(); // converter um list para tipo array
+        }
+
+
+
+        public void ExpandArvore(TreeNode node)
+        {
+            try
+            {
+                //string[] directories = Directory.GetDirectories(node.Value);
+                string[] directories = GetOUs(node.Value);
+
+
+                foreach (string directory in directories)//cria uma var que vai percorrer o array listado acima
+                {
+                    TreeNode childNote = new TreeNode();
+
+                    //childNote.Text = directory.Split('\\')[directory.Split('\\').Length -1];
+                    childNote.Text = directory.Split(',')[0].Split('=')[1];
+
+                    if (directory.StartsWith("OU"))
+                    {
+                        childNote.ImageUrl = "~/imagens/GRUPO.png";
+
+                    }
+                    else if (directory.StartsWith("CN"))
+                    {
+                        childNote.ImageUrl = "~/imagens/USUARIO.png";
+
+                    }
+
+                    childNote.Value = directory;
+                    node.ChildNodes.Add(childNote);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        protected void arvore_SelectedNodeChanged(object sender, EventArgs e)
+        {
+            ExpandArvore((sender as TreeView).SelectedNode); //chamada generica
+        }
+    }
+}
